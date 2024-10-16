b6669fc9ca663aec8a7c76306830763beaa04d57
kaushikghoraifromss <sciencestudent156@gmail.com>
2024-10-16 22:51:21 +0530

    create mycourses

---
+++ students/mycourses/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>My Courses</title>
+    </head>
+    <body>
+        <div id='mainContainer'>
+            <div id='headingcontainer'>
+                <h1 id='headingmycourse'>My Courses</h1>
+            </div>
+            <div id='searchbar'>
+                <input id='searchcourse' name='searchcourse' type='text' placeholder='search Courses'>
+            </div>
+        </div>
+    </body>
+</html>
